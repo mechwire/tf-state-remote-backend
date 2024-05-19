d6b77ed956b96cc2b1ab3048315df957d29aec54
mechwire <jncmaguire@gmail.com>
2024-05-19 14:56:40 -0400

    remove refresh-only

--- infra/main.tf
+++ infra/main.tf
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "tf_state_bucket_objects" {
     }
 
     actions   = ["s3:GetObject", "s3:PutObject"]
-    resources = ["${aws_s3_bucket.tf_state.arn}//*/repo-setup.tf_state"]
+    resources = ["${aws_s3_bucket.tf_state.arn}/*/repo-setup.tf_state"]
   }
 }
 
